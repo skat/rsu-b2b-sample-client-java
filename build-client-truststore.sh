cb4694b4926fdfaa023213c7ec463c104086521e
cdcurry <cdcurry@krispoint.com>
2021-01-29 14:14:53 +0100

    Removed old TLS/SSL certs. from truststore

--- build-client-truststore.sh
+++ build-client-truststore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CLIENT_TUSTSTORE=src/main/resources/keystore/client-truststore.jks
+CLIENT_TUSTSTORE=rsu-b2b-sample-client/src/main/resources/keystore/client-truststore.jks
 
 echo "Before import ..."
 keytool -list -keystore $CLIENT_TUSTSTORE -storepass storepassword
@@ -12,17 +12,15 @@ keytool -noprompt -import -value skatserver -file pem/b2b-test-system.pem -keyst
 # Import SSL cert for secure transport (https://..)
 # The full cert. chain is viewable with openssl command: openssl s_client -connect host:port -showcerts
 #
-keytool -noprompt -import -value ssl_chain_0 -file pem/ssl-chain-0.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -noprompt -import -alias ssl_chain_0 -file pem/ssl-chain-0.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
 # Level 0 is sufficient for completeness we import the full chain (level 1,2,3..)
-keytool -noprompt -import -value ssl_chain_1 -file pem/ssl-chain-1.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
-keytool -noprompt -import -value ssl_chain_2 -file pem/ssl-chain-2.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
+keytool -noprompt -import -alias ssl_chain_1 -file pem/ssl-chain-1.pem -keystore $CLIENT_TUSTSTORE -storepass storepassword
 
 echo "After import ..."
 keytool -list -keystore $CLIENT_TUSTSTORE -storepass storepassword
 
 ### Reverse imports
 ### NOT ACTIVE
-# keytool -delete -value skatserver -keystore src/main/resources/keystore/client-truststore.jks -storepass storepassword
-# keytool -delete -value ssl_chain_0 -keystore src/main/resources/keystore/client-truststore.jks -storepass storepassword
-# keytool -delete -value ssl_chain_1 -keystore src/main/resources/keystore/client-truststore.jks -storepass storepassword
-# keytool -delete -value ssl_chain_2 -keystore src/main/resources/keystore/client-truststore.jks -storepass storepassword
+# keytool -delete -alias skatserver -keystore rsu-b2b-sample-client/src/main/resources/keystore/client-truststore.jks -storepass storepassword
+# keytool -delete -alias ssl_chain_0 -keystore rsu-b2b-sample-client/src/main/resources/keystore/client-truststore.jks -storepass storepassword
+# keytool -delete -alias ssl_chain_1 -keystore rsu-b2b-sample-client/src/main/resources/keystore/client-truststore.jks -storepass storepassword
