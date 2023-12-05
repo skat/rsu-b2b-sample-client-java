a492f2674956a6c8591f6a0a8e0c410f75af8995
cdcurry <cdcurry@krispoint.com>
2023-12-05 21:54:30 +0100

    Cleanup

--- prep-local-tomcat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -r rsu-b2b-sample-client/src/main/resources/keystore rsu-b2b-sample-client-gui/target/keystore
-cp app.conf rsu-b2b-sample-client-gui/target
-cp start-tomcat.sh rsu-b2b-sample-client-gui/target
--- start-tomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Starting Tomcat. Once it's running open:"
-echo "---"
-echo "http://localhost:8080/rsu-b2b-sample-client-gui"
-echo "---"
-
-java -jar rsu-b2b-sample-client-gui-1.2-war-exec.jar
